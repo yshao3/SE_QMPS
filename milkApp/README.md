e69947fb96813039f1614f3723b4ea63386d0197
Yijun Shao <ys849@cornell.edu>
2018-04-11 14:17:12 -0400

    changes

--- milkApp/README.md
+++
@@ -1,1 +0,0 @@
-This is a mobile app for recording Cornell Milk Quality Testing.
--- milkApp/src/sample-json-data/alignment.ts
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-        farm: 'Cornell Vet Farm',
-        date: '03-12-2018',
-        observer: 'Tom',
-        good: 5,
-        bad: 7
-    }
-];
